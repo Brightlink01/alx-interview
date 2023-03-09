34ae5860954b7e037d315af00fa3a5f35804b370
Brightlink01 <101412150+Brightlink01@users.noreply.github.com>
2023-03-09 12:13:54 -0800

    commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,35 +1,44 @@
 #!/usr/bin/node
-
+/**
+ * a script that prints all characters of a Star Wars movie
+ * use star wars api
+ */
 const request = require('request');
-const { exit } = require('process');
-const filmId = process.argv[2];
-const api = `https://swapi-api.hbtn.io/api/films/${filmId}/`;
-
-if (filmId === undefined) {
-  console.log('Usage: node 0-starwars_characters.js <film id>');
-  exit();
+const film_id = process.argv[2];
+if (!film_id  || isNaN(film_id)) {
+  process.exit(1);
 }
+const url = `https://swapi-api.hbtn.io/api/films/${film_id}`;
 
-// GET film data
-request(api, (error, response, body) => {
+request(url, (error, res, body) => {
   if (error) {
-    console.log('error', error);
-  } else {
-    const data = JSON.parse(body).characters;
-    printSorted(data, 0);
+    console.log(error);
+    return;
   }
-});
+  const characterList = [];
 
-// GET each character in sorted order
-function printSorted (chars, n) {
-  if (n === chars.length) { return; }
-  request(chars[n], (error, response, actors) => {
-    if (error) {
-      console.log('error', error);
-    } else {
-      const actor = JSON.parse(actors);
-      console.log(actor.name);
-      printSorted(chars, n + 1);
-    }
+  const json = JSON.parse(body);
+  const characters = json.characters;
+
+  characters.forEach((character) => {
+    const url = character;
+    const promise = new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        const json = JSON.parse(body);
+        resolve(json.name);
+      });
+    });
+    characterList.push(promise);
   });
-}
+  Promise.all(characterList).then((values) => {
+    values.forEach((value) => {
+      console.log(value);
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
+});
