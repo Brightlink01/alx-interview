acb88c70136978e0e30efe93caea6bb3eab12e63
Brightlink01 <101412150+Brightlink01@users.noreply.github.com>
2023-03-11 10:12:58 -0800

    late commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,35 @@
 #!/usr/bin/node
-/**
- * a script that prints all characters of a Star Wars movie
- * use star wars api
- */
+
 const request = require('request');
-const film_id = process.argv[2];
-if (!film_id  || isNaN(film_id)) {
-  process.exit(1);
+const { exit } = require('process');
+const filmId = process.argv[2];
+const api = `https://swapi-api.hbtn.io/api/films/${filmId}/`;
+
+if (filmId === undefined) {
+  console.log('Usage: node 0-starwars_characters.js <film id>');
+  exit();
 }
-const url = `https://swapi-api.hbtn.io/api/films/${film_id}`;
 
-request(url, (error, res, body) => {
+// GET film data
+request(api, (error, response, body) => {
   if (error) {
-    console.log(error);
-    return;
+    console.log('error', error);
+  } else {
+    const data = JSON.parse(body).characters;
+    printSorted(data, 0);
   }
-  const characterList = [];
-
-  const json = JSON.parse(body);
-  const characters = json.characters;
-
-  characters.forEach((character) => {
-    const url = character;
-    const promise = new Promise((resolve, reject) => {
-      request(url, (error, response, body) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        const json = JSON.parse(body);
-        resolve(json.name);
-      });
-    });
-    characterList.push(promise);
-  });
-  Promise.all(characterList).then((values) => {
-    values.forEach((value) => {
-      console.log(value);
-    });
-  }).catch((error) => {
-    console.log(error);
-  });
 });
+
+// GET each character in sorted order
+function printSorted (chars, n) {
+  if (n === chars.length) { return; }
+  request(chars[n], (error, response, actors) => {
+    if (error) {
+      console.log('error', error);
+    } else {
+      const actor = JSON.parse(actors);
+      console.log(actor.name);
+      printSorted(chars, n + 1);
+    }
+  });
+}
